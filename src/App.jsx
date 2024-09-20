9b918bb5016db9cf8f87760ab6702ad825b0e6ff
Gabriel Ferreira <gabriel.oliveiracrz@gmail.com>
2024-09-19 21:24:18 -0300

    react fundamentals

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <div>
+      <Post
+        author="Gabriel Ferreira"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rem et cum itaque natus eos eaque, aperiam labore ad sunt. Odio beatae fugiat ipsa voluptatum quos accusamus consequatur praesentium dolore!"
+      />
+    </div>
+  )
+}
