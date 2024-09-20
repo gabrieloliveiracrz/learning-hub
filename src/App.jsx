27edd6a7297552a61458cb63f6141efd13ebd23d
Gabriel Ferreira <gabriel.oliveiracrz@gmail.com>
2024-09-19 22:05:49 -0300

    include global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+
+import './global.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Gabriel Ferreira"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rem et cum itaque natus eos eaque, aperiam labore ad sunt. Odio beatae fugiat ipsa voluptatum quos accusamus consequatur praesentium dolore!"
