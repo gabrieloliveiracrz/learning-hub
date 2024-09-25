d1dd7db36a54a0481388e30bb30c1a892d01b483
Gabriel Ferreira <gabriel.oliveiracrz@gmail.com>
2024-09-24 21:59:55 -0300

    comment struct

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.commnent}>
+            <img src="https://avatars.githubusercontent.com/u/93164702?v=4" />
+
+            <div className={styles.commnentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Gabriel Ferreira</strong>
+                            <time
+                                title='22 de Setembro às 9:05h'
+                                dateTime='2024-09-22 09:05:20'
+                            >
+                                Cerca há 1h atrás
+                            </time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    <p>Muito bom, parabéns</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
