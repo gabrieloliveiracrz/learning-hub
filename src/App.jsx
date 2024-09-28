7be04b4b9fbfa201fb3f5891103b24b71d1ec5c6
Gabriel Ferreira <gabriel.oliveiracrz@gmail.com>
2024-09-28 19:51:20 -0300

    start object

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,29 @@ const posts = [
     id: 1,
     author: {
       avatarUrl: 'https://github.com/gabrieloliveiracrz.png',
-      name: 'Gabriel Ferreira'
-    }
+      name: 'Gabriel Ferreira',
+      role: "Web Developer"
+    },
+    content: [
+      {type : 'paragraph', content: 'Fala galeraa 👋'},
+      {type : 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type : 'link', content: 'jane.design/doctorcare'},     
+    ],
+    publishedAt: new Date('2024-09-28 19:45:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/vraulista.png',
+      name: 'João Henrique',
+      role: "Estudante"
+    },
+    content: [
+      {type : 'paragraph', content: 'Fala galeraa 👋'},
+      {type : 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type : 'link', content: 'jane.design/doctorcare'},     
+    ],
+    publishedAt: new Date('2024-09-28 19:45:00')
   }
 ]
 
@@ -24,14 +45,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Gabriel Ferreira"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rem et cum itaque natus eos eaque, aperiam labore ad sunt. Odio beatae fugiat ipsa voluptatum quos accusamus consequatur praesentium dolore!"
-          />
-          <Post
-            author="Gabriel Ferreira"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rem et cum itaque natus eos eaque, aperiam labore ad sunt. Odio beatae fugiat ipsa voluptatum quos accusamus consequatur praesentium dolore!"
-          />
+          {
+            posts.map(post => (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            ))
+          }
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,7 +2,7 @@ import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
-export function Post() {
+export function Post({author, content, publishedAt}) {
     return (
         <article className={styles.post}>
             <header>
@@ -26,14 +26,7 @@ export function Post() {
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋 </p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz  </p>no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-                <p>👉 <a href="">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="">#novoprojeto</a>
-                    <a href=""> #nlw </a>
-                    <a href=""> #rocketseat</a>
-                </p>
+                
             </div>
 
             <form className={styles.commentForm}>
@@ -45,8 +38,7 @@ export function Post() {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
+                
             </div>
         </article>
     )
