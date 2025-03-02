abff5883bd75d53afbc6d0139b3a22fe829b286e
Gabriel Ferreira <gabriel.oliveiracrz@gmail.com>
2025-03-02 13:20:44 -0300

    feat: form subscribe

---
+++ src/app/invite/ranking.tsx
@@ -0,0 +1,69 @@
+import Image from 'next/image'
+
+import medalCooper from '../../assets/medal-cooper.svg'
+import medalGold from '../../assets/medal-gold.svg'
+import medalSilver from '../../assets/medal-silver.svg'
+
+export function Ranking() {
+  return (
+    <div className="w-full max-w-[440px] space-y-5">
+      <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+        Ranking de indicações
+      </h2>
+
+      <div className="space-y-4">
+        <div className="relative rounded-xl bg-gray-700 border-gray-600 p-6 flex flex-col justify-center gap-3">
+          <span className="text-sm text-gray-300 leading-none">
+            <span className="font-semiboldt">1º</span> | Gabriel Ferreira
+          </span>
+
+          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+            1030
+          </span>
+
+          <Image
+            src={medalGold}
+            alt="Medalha de Ouro"
+            className="absolute top-0 right-8"
+          />
+        </div>
+      </div>
+
+      <div className="space-y-4">
+        <div className="relative rounded-xl bg-gray-700 border-gray-600 p-6 flex flex-col justify-center gap-3">
+          <span className="text-sm text-gray-300 leading-none">
+            <span className="font-semiboldt">2º</span> | Gabriel Ferreira
+          </span>
+
+          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+            1030
+          </span>
+
+          <Image
+            src={medalSilver}
+            alt="Medalha de Prata"
+            className="absolute top-0 right-8"
+          />
+        </div>
+      </div>
+
+      <div className="space-y-4">
+        <div className="relative rounded-xl bg-gray-700 border-gray-600 p-6 flex flex-col justify-center gap-3">
+          <span className="text-sm text-gray-300 leading-none">
+            <span className="font-semiboldt">3º</span> | Gabriel Ferreira
+          </span>
+
+          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+            1030
+          </span>
+
+          <Image
+            src={medalCooper}
+            alt="Medalha de Bronze"
+            className="absolute top-0 right-8"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
